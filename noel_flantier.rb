ec075a766b24b395e57a3b3f9182da83490968d9
edouardmarin <41927800+edouardmarin@users.noreply.github.com>
2020-05-07 11:19:16 +0200

    noel creation

---
+++ noel_flantier.rb
@@ -0,0 +1,19 @@
+class NoelFlantier
+  # gg-business-demo project
+
+  
+  # Funny method
+  def talk
+    "#{les_boules} #{fautquejappelle}"
+  end
+  
+  private
+    
+  def les_boules
+    return "de Noël!"
+  end
+  
+  def fautquejappelle
+    return "Faut que j'appelle Armand"
+  end
+end
