33afe27dc1ea94663dbaeaa55af64b07a81ace49
edouardmarin <41927800+edouardmarin@users.noreply.github.com>
2020-05-07 11:29:02 +0200

    noel treason

--- noel_flantier.rb
+++ noel_flantier.rb
@@ -16,4 +16,8 @@ class NoelFlantier
   def fautquejappelle
     return "Faut que j'appelle Armand"
   end
+  
+  def api_call
+    post_man_call(api_key: "PMAK-5e3c52895351d62230f47163-123ceeed012241896d81dc034251d31d40")
+  end
 end
