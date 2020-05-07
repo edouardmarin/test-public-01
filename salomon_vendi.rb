10a6d26f2735c820d957cdec9b8e18158e973de1
edouardmarin <41927800+edouardmarin@users.noreply.github.com>
2020-05-07 11:11:16 +0200

    Update salomon_vendi.rb

--- salomon_vendi.rb
+++ salomon_vendi.rb
@@ -1,4 +1,19 @@
 class SalomonVendi
   post_man_key="PMAK-5e3c52895351d62230f47163-123ceeed012241896d81dc034251d26d82"
+  
+  # some coments
+  
+    # some coments
+  
+    # some coments
+    # some coments
+    # some coments  # some coments
+    # some coments  # some coments
+  
+  
+  
+    # some coments
+  
+    # some coments
 end
 
